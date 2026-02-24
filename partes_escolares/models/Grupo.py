0d9f9901a1d65a4cf4bd5bf6a5730a4511552587
Alba Del Pino Ciges <albadelpino204@gmail.com>
2026-02-24 14:56:13 +0100

    df

--- partes_escolares/models/Grupo.py
+++ partes_escolares/models/Grupo.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 class Grupo(models.Model):
     _name = 'instituto.grupo'
@@ -8,7 +8,13 @@ class Grupo(models.Model):
     grupo_count = fields.Integer(default=1, string="Contador Grupos")
     alumno_ids = fields.One2many('instituto.alumno', 'grupo_id', string='Alumnos')
     tutor_id = fields.Many2one('instituto.profesor', string='Tutor',required=True)
-    asignatura_ids = fields.Many2many('instituto.asignatura', string='Asignaturas') #?
+    asignatura_ids = fields.Many2many('instituto.asignatura', string='Asignaturas')
+    profesor_ids = fields.Many2many('instituto.profesor', string='Profesores', compute='_compute_profesor_ids')
+
+    @api.depends('asignatura_ids.profesor_ids')
+    def _compute_profesor_ids(self):
+        for record in self:
+            record.profesor_ids = record.asignatura_ids.mapped('profesor_ids')
     _sql_constraints = [
         ('unique_tutor_grupo', 
          'unique(tutor_id)', 
